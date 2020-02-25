31726153a6d6439e9f01068f76b562ad3a6a0f17
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2020-02-25 12:44:34 +0330

    api result class

---
+++ Tamtom/Tamtom.API/APIResultModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamtom.API
+{
+    public class APIResultModel
+    {
+        public object Entries { get; set; }
+        public object Result { get; set; }
+        public StatusInfo Status { get; set; }
+    }
+
+    public class StatusInfo
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+
+        public StatusInfo(int code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+    }
+
+}
+
