6d8ca8e2913875438ae656667723ad0734cef25d
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2020-02-25 12:49:35 +0330

    add version

---
+++ Tamtom/Tamtom.API/Version/Version.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tamtom.Database.Dapper.Crud;
+
+namespace Tamtom.API.Version
+{
+    public class Version : Crud, ICrud
+    {
+        public Version() : base("Version", "Application") { }
+
+        #region Not implemented method
+        public Task<int> DeActive(CrudModels.PrimaryKeyID id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeActive(CrudModels.PrimaryKeyGuid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeActiveLanguage(CrudModels.DeActiveLanguageModelWithID model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeActiveLanguage(CrudModels.DeActiveLanguageModelWithGuid model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(CrudModels.PrimaryKeyID id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(CrudModels.PrimaryKeyGuid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteLanguage(CrudModels.DeleteLanguageModelWithID model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteLanguage(CrudModels.DeleteLanguageModelWithGuid model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Insert<InputType>(InputType model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertAndReturnInsertedID<InputType>(InputType model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ReturnType> SelectSingle<ReturnType>(CrudModels.SelectSingleModelWithID model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ReturnType> SelectSingle<ReturnType>(CrudModels.SelectSingleModelWithGuid model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update<InputType>(InputType model)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Tamtom/Tamtom.API/Version/VersionModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamtom.API.Version
+{
+    public class VersionModel
+    {
+        public int VersionID { get; set; }
+        public string ProductName { get; set; }
+        public string Platform { get; set; }
+        public string TargetOS { get; set; }
+        public string PlatformVersion { get; set; }
+        public string Description { get; set; }
+        public bool? IsRequired { get; set; }
+        public bool? Stable { get; set; }
+        public string CompanyName { get; set; }
+        public string Authors { get; set; }
+        public string Language { get; set; }
+        public string Technology { get; set; }
+        public string Key { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
